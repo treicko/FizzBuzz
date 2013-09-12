e8946557555cf9dc8259c8552b175d95eea3558a
Israel Antezana R <israelantezana@gmail.com>
2013-09-12 07:48:03 -0400

    Se anadio junit a maven

---
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package fizzbuzz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
