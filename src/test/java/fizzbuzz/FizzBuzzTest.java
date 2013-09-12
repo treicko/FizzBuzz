96ba7ec73021e4835f774e5b6c95b3e34f1c1024
Israel Antezana R <israelantezana@gmail.com>
2013-09-12 08:06:43 -0400

    Se refactorizo pruebas

--- src/test/java/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -2,27 +2,31 @@ package fizzbuzz;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
+	FizzBuzz fb;
+	
+	@Before
+	public void setup() {
+		fb = new FizzBuzz();
+	}
 
 	@Test
 	public void imprimeFizzSiNumeroEs3() {
-		FizzBuzz fb = new FizzBuzz();
 		assertEquals("Fizz",fb.generar(3));
 	}
 	
 	@Test
 	public void imprimeElMismoNumeroSiNoEs3()
 	{
-		FizzBuzz fb = new FizzBuzz();
 		assertEquals("4",fb.generar(4));
 	}
 	
 	@Test
 	public void imprimeFizzSiEsMultiploDe3()
 	{
-		FizzBuzz fb = new FizzBuzz();
 		assertEquals("Fizz",fb.generar(6));
 	}
 
