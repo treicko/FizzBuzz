54f2f6717fe8b85d0d8d3fa365f9db415e8775d9
Israel Antezana <israelantezana@gmail.com>
2013-09-17 08:12:53 -0400

    Refactorizaciones varias en logica de negocio

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -2,14 +2,24 @@ package fizzbuzz;
 
 public class FizzBuzz {
 
+	private static final int _FIZZ = 3;
+	private static final int _BUZZ = 5;
+	private static final String _MENSAJE_BUZZ = "Buzz";
+	private static final String _MENSAJE_FIZZ = "Fizz";
+	private static final String _MENSAJE_FIZZ_BUZZ = "FizzBuzz";
+
 	public String generar(int numero) {
-		if ((numero % 3 == 0)&&(numero % 5 == 0))
-			return "FizzBuzz";
-		if (numero % 3 == 0)
-			return "Fizz";
-		if (numero % 5 == 0)
-			return "Buzz";
+		if (esMultiploDe(numero, _FIZZ)&&esMultiploDe(numero, _BUZZ))
+			return _MENSAJE_FIZZ_BUZZ;
+		if (esMultiploDe(numero, _FIZZ))
+			return _MENSAJE_FIZZ;
+		if (esMultiploDe(numero, _BUZZ))
+			return _MENSAJE_BUZZ;
 		return Integer.toString(numero);			
+	}
+
+	private boolean esMultiploDe(int numero, int multiplo) {
+		return numero % multiplo == 0;
 	}
 
 }
